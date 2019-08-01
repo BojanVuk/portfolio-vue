05daead05ec3bb753104ea5264b6863e3b608dd7
Bojan Vukovic <vbojan2@gmail.com>
2019-08-01 17:28:37 +0200

    REFACTOR: Changing some things suitable for project

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Task from './views/Task.vue'
 
 Vue.use(Router)
 
@@ -12,10 +11,5 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/tasks/:id',
-      name: 'task',
-      component: Task
-    }
   ]
 })
