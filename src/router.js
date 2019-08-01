3936af75ad0fe199f0f81d8947291b3540ceda40
Bojan Vukovic <vbojan2@gmail.com>
2019-08-01 16:53:11 +0200

    ADD: js files

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Task from './views/Task.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/tasks/:id',
+      name: 'task',
+      component: Task
+    }
+  ]
+})
